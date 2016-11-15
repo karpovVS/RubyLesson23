b13a8d216494c2532939f869613a10cb01a6cb8f
kazankarpov <kazankarpov@gmail.com>
2016-11-15 14:30:41 +0300

    Add post params

--- app.rb
+++ app.rb
@@ -14,6 +14,23 @@ get'/visit' do
 	erb :visit
 end
 
+post '/visit' do #обработка запроса
+
+@username = params[:username]
+@contact_phone = params[:contact_phone]
+@visit_time = params[:visit_time]
+@barber = params[:barber]
+
+
+
+erb "OK,username is #{@username},
+phone number #{@contact_phone}, 
+time #{@visit_time} Цурюльнег будет #{@barber}!"
+
+end
+
+
+
 #get '/contacts' do
  # erb :contacts
 #end`
